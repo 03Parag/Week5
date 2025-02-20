367e52a9956069e29023c25c16a3b4a59cc4b306
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-20 10:58:26 +0530

    [Parag Agrawal] Added problem to create a JSON object for a Student with fields: name, age, and subjects (array)

---
+++ Day02_JSON/StudentJson.java
@@ -0,0 +1,19 @@
+package com.Week5.Day02_JSON;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.util.Arrays;
+
+// Create a JSON object with fields name, age and subject
+class StudentJson {
+    public static void main(String[] args) {
+        // Create JSON object and put element
+        JSONObject studentJson = new JSONObject();
+        studentJson.put("name", "John Doe");
+        studentJson.put("age", 20);
+        studentJson.put("subjects", new JSONArray(Arrays.asList("Math", "Science", "History")));
+
+        // Printing JSON object
+        System.out.println(studentJson);
+    }
+}
+
